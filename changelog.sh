b337256c6044938e011da00158150b1e562afeb0
vencik <vencik@razdva.cz>
2013-08-11 14:13:21 +0200

    Change log added to project documentation
    changelog.sh now also generates project documentation worthy
    version of the change log.
    HTTP form is generated by asciidoc and linked from the index.

--- changelog.sh
+++ changelog.sh
@@ -1,8 +1,12 @@
 #!/bin/sh
 
 this=`basename $0`
+date=`date`
 
 log_file="CHANGELOG"
+doc_file="doc/changelog.txt"
+
+commit_url_prefix="https://github.com/vencik/ctx-fryer/commit/"
 
 
 # Change log preamble
@@ -10,14 +14,26 @@ cat > ${log_file} <<HERE
 CTX Fryer Project Change Log
 ============================
 
-The change log was generated on `date`
+The change log was generated on ${date}
 by the ${this} script.
 Below, the committed changes appear in reversed order (most recent first).
 Commit hashes are provided to simplify search for the particular commit;
 just append the hash to the following URL:
-https://github.com/vencik/ctx-fryer/commit/
+${commit_url_prefix}
 
 HERE
+
+# Change log (for project documentation) preamble
+cat > ${doc_file} <<HERE
+CTX Fryer Project Change Log
+----------------------------
+
+The change log was generated on ${date}.
+
+Below, the committed changes appear in reversed order (most recent first).
+
+HERE
+
 
 # Transform git log into change log
 format="%s%x00%H%x00%cN%x00%cE%x00%cd%x00%b%x01"
@@ -35,6 +51,7 @@ BEGIN {
 {
     sub("^\n", "");
 
+    # Plain text file output
     ul = "";
     for (i = 0; i < length($1); ++i)
         ul = ul "-";
@@ -42,11 +59,34 @@ BEGIN {
     print "\n" $1 "\n" ul "\n" \
         "Date: " $5 "\n" \
         "By:   " $3 "  <" $4 ">\n" \
-        "Hash: " $2 "\n";
+        "Hash: " $2 "\n" \
+        >> "'${log_file}'";
 
-    if (length($6)) print $6;
+    if (length($6)) print $6 >> "'${log_file}'";
+
+    # Project documentation output
+    heading = "link:'${commit_url_prefix}'" $2 "[" $1 "]";
+
+    ul = "";
+    for (i = 0; i < length(heading); ++i)
+        ul = ul "*";
+
+    print "\n." heading "\n*" ul "\n" \
+        $5 " by '\''" $3 "'\''\n\n_Hash_: " $2 \
+        >> "'${doc_file}'";
+
+    if (length($6)) {
+        text = $6;
+
+        sub("\n$", "", text);
+        gsub("\n", "\n ", text);
+
+        print "\n " text >> "'${doc_file}'";
+    }
+
+    print "*" ul >> "'${doc_file}'";
 }
-' >> ${log_file}
+'
 
 # Commit the new change log
-git commit -m "#changelog" ${log_file}
+git commit -m "#changelog" ${log_file} ${doc_file}

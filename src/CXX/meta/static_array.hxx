d7d736aa5f15c2422416c8c900de4e4c3fd4d701
vencik <vencik@razdva.cz>
2014-06-05 14:21:24 +0200

    meta::static_array template class implemented
    Using C++11, the class allows for deffinition of static arrays
    initialised on compile time by meta-functor.
    Integral base types supported only (so far).
    
    Primary purpose: to be used for pre-computation of bit-related info
    in bytes (set bit count, MSb/LSb position etc).

---
+++ src/CXX/meta/static_array.hxx
@@ -0,0 +1,107 @@
+#ifndef meta__static_array_hxx
+#define meta__static_array_hxx
+
+/**
+ *  \brief  Static array with template initialiser
+ *
+ *  Templates allowing for static arrays compile-time initialisation.
+ *  Uses C++11 variadic templates.
+ *
+ *  \author  Vaclav Krpec  <vencik@razdva.cz>
+ *  \date    2014/06/05
+ *
+ *  Legal notices
+ *
+ *  Copyright 2014 Vaclav Krpec
+ *
+ *  CTX Fryer is free software: you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation, either version 3 of the License, or
+ *  (at your option) any later version.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#include "config.hxx"
+
+#include <stdexcept>
+
+
+namespace meta {
+
+#ifdef HAVE_CXX11  // C++11 required for variadic templates
+
+/** Type base */
+template <typename T>
+class base {
+    private:
+
+    /** Compile-time initialised array (implementation) */
+    template <T... args>
+    struct static_array_impl {
+        static const T data[sizeof...(args)];  /**< Data */
+    };  // end of template class static_array_impl
+
+    /** Initialiser */
+    template <size_t N, template <size_t> class F, T... args>
+    struct static_array_init {
+        typedef
+            typename static_array_init<N - 1, F, F<N>::value, args...>::values
+            values;
+    };  // end of template struct static_array_init
+
+    /** Initialiser (recursion fixed point) */
+    template <template <size_t> class F, T... args>
+    struct static_array_init<0, F, args...> {
+        typedef static_array_impl<F<0>::value, args...> values;
+    };  // end of template struct static_array_init<0, ...>
+
+    public:
+
+    /**
+     *  \brief  Compile-time initialised array
+     *
+     *  \tparam  T  Base type
+     *  \tparam  N  Array size
+     *  \tparam  F  Initialisation meta-functor
+     */
+    template <size_t N, template <size_t> class F>
+    class static_array {
+        private:
+
+        /** Implementation */
+        typedef typename static_array_init<N - 1, F>::values impl_t;
+
+        public:
+
+        /** Static array accessor */
+        inline static const T & at(size_t i) throw(std::range_error) {
+            if (!(i < N))
+                throw std::range_error(
+                    "meta::base::static_array: out of range");
+
+            return impl_t::data[i];
+        }
+
+        /** Array accessor */
+        inline const T & operator [] (size_t i) const { return at(i); }
+
+    };  // end of template struct static_array
+
+};  // end of template struct base
+
+// static array implementation initialisation
+template <typename T> template<T... args>
+const T base<T>::static_array_impl<args...>::data[sizeof...(args)] = { args... };
+
+#endif  // end of #ifdef HAVE_CXX11
+
+}  // end of namespace meta
+
+#endif  // end of #ifndef meta__static_array_hxx
---
+++ src/CXX/unit_test/meta/static_array.cxx
@@ -0,0 +1,98 @@
+/**
+ *  \brief  Compile-time initialised array unit test
+ *
+ *  \author  Vaclav Krpec  <vencik@razdva.cz>
+ *  \date    2014/06/05
+ *
+ *  Legal notices
+ *
+ *  Copyright 2014 Vaclav Krpec
+ *
+ *  CTX Fryer is free software: you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation, either version 3 of the License, or
+ *  (at your option) any later version.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#include "meta/static_array.hxx"
+
+#include <iostream>
+
+
+/** Array initialiser meta-functor for meta::static_array unit test */
+template <size_t N>
+class array_init {
+    public:
+
+    enum { value = 2 * N };  /**< F(X) == 2*X */
+
+    inline static unsigned check(unsigned n) { return 2 * n; }
+
+};  // end of template class array_init
+
+static const size_t array_size = 876;  // note that the default limit is 900
+
+/** Array initialised at compile time by template initialiser */
+static meta::base<unsigned>::static_array<array_size, array_init> array;
+
+/** \ref meta::static_array unit test */
+int test_static_array() {
+    int exit_code = 0;
+
+    for (size_t i = 0; i < array_size; ++i) {
+        unsigned expected = array_init<array_size>::check(i);
+
+        if (array[i] != expected) {
+            std::cerr
+                << "meta::static_array UT: eq[ " << i << "]: expected "
+                << expected << ", got " << array[i] << std::endl;
+
+            exit_code = 1;
+        }
+    }
+
+    return exit_code;
+}
+
+
+/** Smart pointers unit test */
+int main_impl(int argc, char * const argv[]) {
+    int exit_code = 0;
+
+    // Test static_array
+    exit_code = test_static_array();
+
+    if (0 != exit_code) return exit_code;
+
+    return 0;  // all tests passed
+}
+
+/** Exception-safeness wrapper */
+int main(int argc, char * const argv[]) {
+    int exit_code = 127;  // exception caught
+
+    try {
+        exit_code = main_impl(argc, argv);
+    }
+    catch (std::exception & x) {
+        std::cerr
+            << "Standard exception caught: "
+            << x.what()
+            << std::endl;
+    }
+    catch (...) {
+        std::cerr
+            << "Unknown exception caught"
+            << std::endl;
+    }
+
+    ::exit(exit_code);
+}
